845a331499dfd3541281aafbeba02c052f66a5f9
vasil <vasil.bazhiv@global-main.com>
2023-12-23 18:46:25 +0200

    test

--- src/components/ListHeader/index.tsx
+++ src/components/ListHeader/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { AppSelector } from '../../redux/hook'
-import './ListHeader.Module.scss'
+import React from 'react';
+import { AppSelector } from '../../redux/hook';
+import './ListHeader.Module.scss';
 const ListHeader = () => {
-	const listItems = AppSelector(state => state.ToDo.listItems)
+	const listItems = AppSelector(state => state.ToDo.listItems);
 	return (
 		<div className='list-header'>
-			<h4 className='list-header__title'>Todos ({listItems.length})</h4>
+			<h4 className='list-header__title'>Todos ({listItems.length})12</h4>
 		</div>
-	)
-}
+	);
+};
 
-export default ListHeader
+export default ListHeader;
